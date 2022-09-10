2dbd0b7f1416c4890a312d8c4315d477a5ddec51
danhan <danhandev@gmail.com>
2022-09-11 03:08:37 +0900

    [FEAT]: Connect RDS

--- Backend/Eatpository/settings.py
+++ Backend/Eatpository/settings.py
@@ -133,10 +133,24 @@ WSGI_APPLICATION = 'Eatpository.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',  # engine: mysql
+        'NAME': 'eatpository_db',  # DB Name
+        'USER': 'root',  # DB User
+        'PASSWORD': 'eatpository',  # Password
+        'HOST': '15.165.226.61',
+        'PORT': '3306',  # 데이터베이스 포트
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
